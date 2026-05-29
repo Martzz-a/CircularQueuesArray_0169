debe2048c8fbaab04fb05c97c9487b6eff2b7472
Martzz-a <maritzafidelyah@gmail.com>
2026-05-29 18:51:01 +0700

    Menambahkan If untuk Mengecek Apakah Queue

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -18,5 +18,11 @@ public:
 
         cout << "Enter a number : ";
         cin >> num;
+
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
     }
 };
